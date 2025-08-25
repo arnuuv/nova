9f36e6f32507688a3304a4e9a7eb54a178737d57
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-08-25 13:27:20 +0530

    graph structure

--- 2_access_scraper/main.py
+++ 2_access_scraper/main.py
@@ -1,6 +1,93 @@
-def main():
-    print("Hello from 2-access-scraper!")
+from dotenv import load_dotenv
+from typing import Annotated
+from langgraph.graph import START, END, StateGraph
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+llm = init_chat_model("gpt-4o")
 
 
-if __name__ == "__main__":
-    main()
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+    user_question: str | None
+    google_results: str | None
+    bing_results: str | None
+    reddit_Results: str | None
+    selected_reddit_urls: list[str] | None
+    reddit_post_data: list | None
+    google_analysis: str | None
+    bing_analysis: str | None
+    reddit_analysis: str | None
+    final_answer: str | None
+
+
+def google_search(state: State):
+    return
+
+
+def bing_search(state: State):
+    return
+
+
+def reddit_search(state: State):
+    return
+
+
+def analyze_reddit_posts(state: State):
+    return
+
+
+def retrieve_reddit_posts(state: State):
+    return
+
+
+def analyze_google_results(state: State):
+    return
+
+
+def analyze_bing_results(state: State):
+    return
+
+
+def analyze_reddit_results(state: State):
+    return
+
+
+def synthesize_analyses(state: State):
+    return
+
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("google search", google_search)
+
+graph_builder.add_node("reddit search", reddit_search)
+graph_builder.add_node("bing search", bing_search)
+graph_builder.add_node("analyze_reddit_posts", analyze_reddit_posts)
+graph_builder.add_node("retrieve_retrieve_posts", retrieve_reddit_posts)
+graph_builder.add_node("analyze_google_results", analyze_google_results)
+graph_builder.add_node("analyze_bing_results", analyze_bing_results)
+graph_builder.add_node("analyze_reddit_results", analyze_reddit_results)
+graph_builder.add_node("synthesize_analyses", synthesize_analyses)
+
+graph_builder.add_edge(START, "google_search")
+graph_builder.add_edge(START, "bing_search")
+graph_builder.add_edge(START, "reddit_search")
+
+graph_builder.add_edge("google_search", "analyze_reddit_posts")
+graph_builder.add_edge("bing_search", "analyze_reddit_posts")
+graph_builder.add_edge("reddit_search", "analyze_reddit_posts")
+graph_builder.add_edge("analyze_reddit_posts", "retrieve_reddit_posts")
+
+graph_builder.add_edge("retrieve_reddit_posts", "analyze_google_results")
+graph_builder.add_edge("retrieve_reddit_posts", "analyze_bing_results")
+graph_builder.add_edge("retrieve_reddit_posts", "analyze_reddit_results")
+
+graph_builder.add_edge("analyze_google_results", "synthesize_analyses")
+graph_builder.add_edge("analyze_bing_results", "synthesize_analyses")
+graph_builder.add_edge("analyze_reddit_results", "synthesize_analyses")
+
+graph_builder.add_edge("synthesize_analyses", END)
